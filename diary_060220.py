d0b21c6a0fa40cb585bcc41765565534393e7902
user <edvardsamelko@gmail.com>
2020-02-06 11:53:08 +0200

    20200206_1155

---
+++ diary_060220.py
@@ -0,0 +1,128 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print (4)
+4
+>>> 
+>>> type('Hello, World!')
+<class 'str'>
+>>> type (17)
+<class 'int'>
+>>> tyoe(3.2)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    tyoe(3.2)
+NameError: name 'tyoe' is not defined
+>>> type(3.2)
+<class 'float'>
+>>> type('17')
+<class 'str'>
+>>> print(1,000,000)
+1 0 0
+>>> message = 'bla blu bli'
+>>> n = 17
+>>> pi = 3.14
+>>> print(n)
+17
+>>> print(pi)
+3.14
+>>> type(message)
+<class 'str'>
+>>> type(n)
+<class 'int'>
+>>> type(pi)
+<class 'float'>
+>>> 76trombones = 'big parade'
+SyntaxError: invalid syntax
+>>> class = 'A T Z'
+SyntaxError: invalid syntax
+>>> print(1)
+1
+>>> x=2
+>>> print(x)
+2
+>>> nano
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    nano
+NameError: name 'nano' is not defined
+>>> print(1)	x=2  print(x)
+SyntaxError: invalid syntax
+>>> nano print
+SyntaxError: invalid syntax
+>>> nano script
+SyntaxError: invalid syntax
+>>> nano tes
+SyntaxError: invalid syntax
+>>> minute = 59
+>>> minute/60
+0.9833333333333333
+>>> minute =59
+>>> minute//60
+0
+>>> 1 + 1
+2
+>>> 5
+5
+>>> x = 5
+>>> x + 1
+6
+>>> quotient = 7//3
+>>> print(quotient)
+2
+>>> remainder= 7&3
+>>> print(remainder)
+3
+>>> remainder = 7%3
+>>> print remainder
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(remainder)?
+>>> print(remainder)
+1
+>>> first = 10
+>>> second = 15
+>>> print(first+second)
+25
+>>> first='100'
+>>> second='150'
+>>> print(first+second)
+100150
+>>> first ='test'
+>>> second = 3
+>>> print(first*second)
+testtesttest
+>>> 
+>>> 
+>>> inp = input()
+si so si
+>>> print(inp)
+si so si
+>>> 
+>>> 
+>>> name=input('ka tevi sauc?\n')
+ka tevi sauc?
+Ed
+>>> print(name)
+Ed
+>>> 
+>>> 
+>>> prompt= 'kads... ir tilpuma koef?\n'
+>>> tilpums = input(prompt)
+kads... ir tilpuma koef?
+17
+>>> int(tilpums)
+17
+>>> int(tilpums) +5
+22
+>>> tilpums = input(prompt)
+kads... ir tilpuma koef?
+kas?
+>>> int(speed)
+Traceback (most recent call last):
+  File "<pyshell#66>", line 1, in <module>
+    int(speed)
+NameError: name 'speed' is not defined
+>>> bad name = 5
+SyntaxError: invalid syntax
+>>> month = 09
+SyntaxError: invalid token
+>>> 
